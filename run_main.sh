d65e9586f8c0e614509571800c6e2849f067eff9
baokui <sy880115>
2020-03-24 23:36:10 +0800

    train code

--- run_main.sh
+++ run_main.sh
@@ -5,4 +5,5 @@
 #nohup /root/anaconda3/envs/pytorch/bin/python3.6 main.py -m head >> log/head.log 2>&1 &
 
 export CUDA_VISIBLE_DEVICES=1
-nohup /root/anaconda3/envs/pytorch/bin/python3.6 main.py -m train >> log/train-emb.log 2>&1 &
+#nohup /root/anaconda3/envs/pytorch/bin/python3.6 main.py -m train >> log/train-emb.log 2>&1 &
+nohup /root/anaconda3/envs/pytorch/bin/python3.6 main.py -m head >> log/head-emb.log 2>&1 &
