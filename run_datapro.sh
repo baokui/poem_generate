e17000dd95cbfaa62d4df7e9c6773947dc777821
baokui <sy880115>
2020-03-24 19:13:39 +0800

    get rawdata

--- run_datapro.sh
+++ run_datapro.sh
@@ -1,5 +1,4 @@
 path_source=/search/odin/guobk/vpa/Poetry-master
-path_target=./data
+path_target=./data/poetryAll.txt
 mode=rawPeom
-mkdir data
 nohup python -u datapro.py $mode $path_source $path_target >> log/datapro.log 2>&1 &
